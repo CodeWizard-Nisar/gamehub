1c67666edfc15db17bb953fb0816ca2a9cb71949
Inoxentkhan <m.nisarshinwar29@gmail.com>
2023-10-29 00:05:14 +0500

    undo changes

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,42 +1,30 @@
-// import React from "react";
-// import { Game } from "../Hooks/useGames";
-// import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
-// import PlatformIconList from "./PlatformIconList";
-// import CriticScore from "./CriticScore";
-// import getCroppedImageUrl from "../services/image-url";
-// import Emogi from "./Emoji";
-
-// interface Props {
-//   game: Game;
-// }
-// const GameCard = ({ game }: Props) => {
-//   return (
-//     <Card>
-//       <Image src={getCroppedImageUrl(game.background_image)} />
-//       <CardBody>
-//         <HStack justifyContent="space-between" marginBottom={3}>
-//           <PlatformIconList
-//             platforms={game.parent_platforms.map((p) => p.platform)}
-//           />
-//           <CriticScore score={game.metacritic}></CriticScore>
-//         </HStack>
-//         <Heading fontSize={"2xl"}>
-//           {game.name} <Emogi rating={game.rating_top}></Emogi>
-//         </Heading>
-//       </CardBody>
-//     </Card>
-//   );
-// };
-
-// export default GameCard;
-import { Text } from "@chakra-ui/react";
 import React from "react";
+import { Game } from "../Hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
-const GameCard = () => {
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
   return (
-    <Text display="center" fontSize="50xl">
-      I Love you
-    </Text>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
+        <Heading fontSize={"2xl"}>
+          {game.name} <Emoji rating={game.rating_top}></Emoji>
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
